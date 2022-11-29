ee60f5dfe322784f5df93ab137aeffc5cb876bdb
Ilya Vitalevich <aliceicelie@gmail.com>
2022-11-30 00:55:03 +0300

    fixed bugs

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -9,20 +9,17 @@ export const progressionGame = () => {
         const arithmeticProgression  = (n, lim) => Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n );
         let arr = arithmeticProgression(randomNumber, randomNumber2);
         if (arr.length === 2) arr = arithmeticProgression(randomNumber, randomNumber2);
-        console.log(arr);
         do {
             randomNumber = randomNum();
             randomNumber2 = randomNum();
             arr = arithmeticProgression(randomNumber, randomNumber2);
         } while (arr.length < 5);
-        console.log(arr);
         const randomIndex = Math.floor(Math.random() * arr.length);
         const item = arr[randomIndex];
         const getRandomItem = (arr) => arr.join(" ").replace(item, "..");
-        getRandomItem(arr);
         const ask = `${getRandomItem(arr)}`;
         const correct = String(item);
-        return [correct, ask];
+        return [correct, String(ask)];
     };
     engine(rule, test);
 };
