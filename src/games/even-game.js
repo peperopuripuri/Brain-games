c8784aabb7048bdd1d3768003cdcb27a853e550d
Ilya Vitalevich <aliceicelie@gmail.com>
2022-11-29 22:34:21 +0300

    fixed hexlet error

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -4,7 +4,7 @@ export const evenGame = () => {
     const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
     const test = () => {
         const randomNumber = randomNum();
-        const ask = randomNumber;
+        const ask = `${randomNumber}`;
         let correct;
         (randomNumber % 2 === 0) ? correct = 'yes' : correct = 'no';
         return [correct, ask];
