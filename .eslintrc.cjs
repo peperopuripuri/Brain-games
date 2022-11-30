76d0f85fb33a77600a300cc3b0226daa941df29f
ilya <aliceicelie@gmail.com>
2022-11-30 23:37:53 +0300

    fix linter and other files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    	"semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
