ba9878b9e9d98901d12f11330b72195ae34ae761
Alice <aliceicelie@gmail.com>
2022-12-05 03:49:25 +0300

    fixed bugs

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -3,28 +3,31 @@ import randomNum from '../helpers.js';
 
 const mainRule = 'What number is missing in the progression?';
 
-const makeProgression = (randomNumber, randomNumber2) => {
-  const doMath = (n, lim) => Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
-  if (randomNumber === randomNumber2) randomNumber = randomNum();
-  if (randomNumber === 0 || randomNumber2 === 0) randomNumber = randomNum();
-  let arr = '';
-  do {
-    randomNumber = randomNum();
-    randomNumber2 = randomNum();
-    arr = doMath(randomNumber, randomNumber2);
-    if (arr.length > 200 || arr.length < 0) arr = doMath(randomNumber, randomNumber2);
-  } while (arr.length < 5);
-  return arr;
+
+const makeProgression = (a, b) => {
+  if (a === b) a = randomNum();
+  if (a === 0 || b === 0) a = randomNum();
+  
+  return Array.from({ length: Math.ceil(b / a) }, (_, i) => (i + 1) * a );
 };
 
 const verify = () => {
-  const progressionArr = makeProgression();
+  let a = randomNum();
+  let b = randomNum();
+  let progressionArr = makeProgression(a, b);
+  do {
+    a = randomNum();
+    b = randomNum();
+    progressionArr = makeProgression(a, b);
+  } while (progressionArr.length < 5)
+  
   const randomIndex = Math.floor(Math.random() * progressionArr.length);
   const item = progressionArr[randomIndex];
   const replaceItem = () => progressionArr.join(' ').replace(item, '..');
 
   const mainQuestion = `${replaceItem()}`;
   const correctAnswer = String(item);
+
   return [correctAnswer, mainQuestion];
 };
 
