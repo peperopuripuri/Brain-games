53d6bdf9255a54a77ac42f567fe4a7a353e8ffdf
Alice <aliceicelie@gmail.com>
2022-12-10 12:56:00 +0300

    fixed bugs

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -3,22 +3,22 @@ import randomNum from '../helpers.js';
 
 const mainRule = 'What number is missing in the progression?';
 
-const makeProgression = (num1) => {
-  let a = num1;
+const makeProgression = (numb) => {
+  let n = numb;
   const result = [];
   const length = 6;
+  const interval = Math.floor(Math.random() * 30);
   for (let i = 1; i < length; i += 1) {
-    a += a;
-    result.push(a);
+    n += interval;
+    result.push(n);
   }
 
   return result.sort((c, d) => c - d);
 };
 
 const generateGameVariables = () => {
-  const a = randomNum();
-  const progressionArr = makeProgression(a);
-  
+  const n = randomNum();
+  const progressionArr = makeProgression(n);
   const randomIndex = Math.floor(Math.random() * progressionArr.length);
   const item = progressionArr[randomIndex];
   const userAnswer = () => progressionArr.join(' ').replace(item, '..');
